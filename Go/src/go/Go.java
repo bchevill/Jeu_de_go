22cbb6cf7ffa42720bf89f97df8d5fac46f45c37
Olivier <oliv.nguyen@hotmail.fr>
2015-01-08 11:21:43 +0100

    Redaction du tour de Jeu (Fonction principale)

--- Go/src/go/Go.java
+++ Go/src/go/Go.java
@@ -5,6 +5,9 @@
  */
 package go;
 
+import java.awt.geom.Point2D;
+import java.util.*;
+
 /**
  *
  * @author NguyenQuoc
@@ -14,8 +17,103 @@ public class Go {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+
+        int taille = 0;
+
+        // Réponse à la question "Voulez-vous passer votre tour"
+        boolean reponsePasserTourJ1 = false, reponsePasserTourJ2 = false;
+
+        // On demande à l'utilisateur la taille du plateau
+        while (taille > 0 && taille < 20)
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Rentrer la taille du plateau de jeu : (En entier) :");
+            taille = scanner.nextInt();
+        }
+
+        // On creer les joueurs et le plateau
+        PlateauDeJeu plateau = new PlateauDeJeu(taille);
+
+        Joueur joueur1 = new Joueur("Blanc");
+        Joueur joueur2 = new Joueur("Noir");
+
+        // Joueur fictif qui désigne le joueur qui est en train de jouer
+        Joueur joueurActif = new Joueur("Gris");
+
+        // Joueur1 est le 1er joueur à jouer
+        joueurActif = joueur1;
+
+        //On vérifie les conditions de fin : Plus d'espace pour jouer ou les 2 joueurs passe leurs tours respectifs
+        while (plateau.resteDegreeLiberte() == false || (reponsePasserTourJ1 == true && reponsePasserTourJ2 == true))
+        {
+
+            //On indique quel joueur doit jouer
+            if (joueurActif.getCouleur().equals("Blanc"))
+            {
+                System.out.println("Début du tour de Joueur Blanc");
+            } else
+            {
+                System.out.println("Début du tour de Joueur Noir");
+            }
+
+            //int qui permet de vérfier que la pose de pierre s'est bien passé
+            int okAjouterPierre = -1;
+
+            //int qui permet de déterminé l'ordonnée de la nouvelle pierre OU d'indiquer que le joueur veut passer son tour (dans ce cas là =-1)
+            int pierreX = -2;
+
+            while (okAjouterPierre != 0 && pierreX != -1)
+            {
+
+                //On demande à l'utilisateur de rentrer l'abcisse de la pierre qu'il veut poser ou s'il veut passer son tour
+                while (pierreX < -1 && pierreX > taille)
+                {
+                    Scanner scannerPosX = new Scanner(System.in);
+                    System.out.println("L'abscisse de la case où vous voulez poser votre pierre : (Entre 0 et " + Integer.toString(taille - 1) + " ou -1 pour passer son tour) :");
+                    pierreX = scannerPosX.nextInt();
+                }
+
+                //Le joueur passe son tour ?
+                if (pierreX == -1)
+                {
+                    //On retient que le joueur passe son tour
+                    if (joueurActif.getCouleur().equals("Blanc"))
+                    {
+                        System.out.println("Le Joueur Blanc passe son tour");
+                        reponsePasserTourJ1 = true;
+                    } else
+                    {
+                        System.out.println("Le Joueur Noir passe son tour");
+                        reponsePasserTourJ2 = true;
+                    }
+                } else
+                {
+
+                    //On demande à l'utilisateur de rentrer l'ordonnée de la pierre qu'il veut poser
+                    int pierreY = -1;
+
+                    while (pierreY < 0 && pierreY > taille)
+                    {
+                        Scanner scannerPosY = new Scanner(System.in);
+                        System.out.println("L'ordonnée de la case où vous voulez poser votre pierre : (Entre 0 et " + Integer.toString(taille - 1) + " ou -1 pour passer son tour) :");
+                        pierreY = scannerPosY.nextInt();
+                    }
+
+                    //On ajoute la pierre au plateau
+                    Point2D position = new Point2D.Double(pierreX, pierreY);
+                    Pierre pierre = new Pierre(joueurActif.getCouleur(), position);
+
+                    // On ajoute la pierre. Si ce n'est pas possible, on boucle
+                    okAjouterPierre = plateau.ajouterPierre(pierre);
+                }
+            }
+
+            //On change de joueur
+            joueurActif = joueurActif.getCouleur().equals("Blanc") ? joueur2 : joueur1;
+        }
+
     }
-    
+
 }
